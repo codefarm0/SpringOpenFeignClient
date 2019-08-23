ef0454b79440cf153f86ec499a6f48fe94132955
Green Learner <greenlearner01@users.noreply.github.com>
2019-08-23 09:58:05 +0530

    adding put, post, delete

--- feignClientDemo/src/main/java/com/gl/feignClientDemo/controller/UserController.java
+++ feignClientDemo/src/main/java/com/gl/feignClientDemo/controller/UserController.java
@@ -29,5 +29,29 @@ public class UserController {
         return userService.getUser(id);
     }
 
+    @GetMapping("userList")
+    public List<User> userList(){
+        logger.info("calling userList() to get list of users");
+        return userService.getUserList();
+    }
+
+
+    @PostMapping("user")
+    public String addUser(@RequestBody User user){
+        logger.info("adding user");
+        return userService.addUser(user);
+    }
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable String id){
+        logger.info("deleting user - {}", id);
+        return userService.deleteUser(id);
+    }
+
+    @PutMapping("/updateAddress/{id}/{newAddress}")
+    String updateAddress(@PathVariable String id, @PathVariable String newAddress){
+        logger.info("updating user - {}", id);
+        return userService.updateAddress(id, newAddress);
+    }
+
 
 }
--- feignClientDemo/src/main/java/com/gl/feignClientDemo/service/UserService.java
+++ feignClientDemo/src/main/java/com/gl/feignClientDemo/service/UserService.java
@@ -6,9 +6,7 @@ import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,20 @@ public interface UserService {
     @GetMapping("/user/{id}")
     User getUser(@PathVariable("id") String id);
 
+    @GetMapping(value = "/users")
+    List<User> getUserList();
 
-}
+
+    @PostMapping("/user")
+    String addUser(User user);
+
+    @DeleteMapping("/user/{id}")
+     String deleteUser(@PathVariable("id") String id);
+
+    @PutMapping("/updateAddress/{id}/{newAddress}")
+     String updateAddress(@PathVariable("id") String id, @PathVariable("newAddress") String newAddress);
+
+
+
+
+    }
